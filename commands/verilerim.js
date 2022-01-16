4122d9f27e417f2a8000e3719e4a865527b0089a
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-01-16 16:57:03 +0300

    Verilerim Hatasi Düzeldi

--- commands/verilerim.js
+++ commands/verilerim.js
@@ -3,19 +3,26 @@ const config =  require("../settings.json")
 const OwnerData = require("../models/ownerdata")
 const TaglıData = require("../models/taglıal");
 exports.execute = async (client, message, args) => {
-  let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])|| message.member;
+  let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
   let embed = new MessageEmbed()
   .setAuthor({ name: message.guild.name, iconURL: message.guild.iconURL({ dynamic: true }) })
   .setThumbnail( message.author.avatarURL({ dynamic: true}))
   .setFooter({ text: config.footer.toString() , iconURL: message.guild.iconURL({ dynamic: true })})
   .setColor("RANDOM")
-  let Db = await OwnerData.findOne({ GuildId: message.guild.id, Member: member.id })
-  let Dbtwo = await OwnerData.findOne({ GuildId: message.guild.id, Member: member.id })
+  let Db = await TaglıData.findOne({ GuildId: message.guild.id, Member: message.member.id })
+  let Dbtwo = await OwnerData.findOne({ GuildId: message.guild.id, Member: message.member.id })
+   if(!member) return message.reply("false")
+    if(Db === null ) {
+      let comp = 15;
+      message.channel.send({ embeds :[embed.setDescription(`${member} Mevcut İlerleminiz \`${Dbtwo.TotalReg}\` Kayıt Durumundasiniz 
   
- if(Dbtwo) {
-   if(Dbtwo.TotalReg <= 15 || Db.TotalTag <= 15 ) {
-    let Db = await TaglıData.findOne({ GuildId: message.guild.id, Member: member.id })
-    let Dbtwo = await OwnerData.findOne({ GuildId: message.guild.id, Member: member.id })
+      ${(await client.createBar(Dbtwo.TotalReg,comp,8)).toString()} Seviye Yükselmek İçin 
+      
+      \`${comp - Dbtwo.TotalReg}\` Taglı Çekmen Gerekiyor
+     
+      Alıcağin Rol <@&${config.OnelevelTag}>`)]});
+    }
+   if( Dbtwo && Db !== null ) {
     let comp = 15;
     message.channel.send({ embeds :[embed.setDescription(`${member} Mevcut İlerleminiz \`${Db.TotalTag}\` Taglı \`${Dbtwo.TotalReg}\` Kayıt Durumundasiniz 
 
@@ -30,16 +37,14 @@ exports.execute = async (client, message, args) => {
     \`${comp - Dbtwo.TotalReg}\` Kişi Kayıt Etmen Gerekiyor
 
     Alıcağin Rol <@&${config.Onelevelreg}> `)]});
-   }
- } else  {
-    message.reply("Veriniz Yok")
- }
+   } 
+
 
 
 
 }
 exports.conf = {
-  command: "me",
+  command: "verilerim",
   description: "",
   aliases: []
 }
