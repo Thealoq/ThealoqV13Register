16f7e61e1bc3daefcc4e3eb3889ef83315f31a5d
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-01-18 21:41:39 +0300

    Update MemberData.js

--- models/MemberData.js
+++ models/MemberData.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const MemberRolData = mongoose.Schema({
-  guildId: String, 
-  member: String,
-  role: String,
-  executor: String,
-  Name: String
-});
+const MemberRolData = mongoose.Schema(
+  {
+    guildId: String,
+    member: String,
+    role: String,
+    executor: String,
+    Name: String,
+  },
+  { minimize: false, collection: "MemberRolData" }
+);
 
 module.exports = mongoose.model("MemberRolData", MemberRolData);
