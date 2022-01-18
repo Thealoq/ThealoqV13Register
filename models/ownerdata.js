89bef2f8cae9e6819f8e1ecf34bca4738f14c222
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-01-18 21:41:40 +0300

    Update ownerdata.js

--- models/ownerdata.js
+++ models/ownerdata.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const Ownerdata = new mongoose.Schema({
-  GuildId: String, 
-  Member: String,
-  TotalReg: Number,
-  WomanReg: Number,
-  ManReg: Number,
-});
+const Ownerdata = new mongoose.Schema(
+  {
+    GuildId: String,
+    Member: String,
+    TotalReg: Number,
+    WomanReg: Number,
+    ManReg: Number,
+  },
+  { minimize: false, collection: "Ownerdata" }
+);
 
 module.exports = mongoose.model("Ownerdata", Ownerdata);
