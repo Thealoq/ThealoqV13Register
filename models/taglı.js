de7dd02e5e09b1e42d71d27949e16ce0a8e0bc25
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-01-18 21:41:49 +0300

    Update taglı.js

--- models/taglı.js
+++ models/taglı.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-const TaglıAlım = mongoose.Schema({
-  guildId: String, 
-  taglıalım: Boolean,
-});
+const TaglıAlım = mongoose.Schema(
+  {
+    guildId: String,
+    taglıalım: Boolean,
+  },
+  { minimize: false, collection: "TaglıAlım" }
+);
 
 module.exports = mongoose.model("TaglıAlım", TaglıAlım);
