ff46f6fd1f9f79ba32e370999c70216544b4ea3c
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-01-18 21:41:55 +0300

    Update taglıal.js

--- models/taglıal.js
+++ models/taglıal.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const TaglıAl = mongoose.Schema({
-  GuildId: String, 
-  Member: String,
-  TotalTag: Number,
-  tagalanlar: Array,
-});
+const TaglıAl = mongoose.Schema(
+  {
+    GuildId: String,
+    Member: String,
+    TotalTag: Number,
+    tagalanlar: Array,
+  },
+  { minimize: false, collection: "TaglıAl" }
+);
 
 module.exports = mongoose.model("TaglıAl", TaglıAl);
